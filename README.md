cf1175d0535f4269dbcf2e3b4c33629d20ec7248
Titas Gailius <titas.gailius@gmail.com>
2018-01-09 12:58:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tomoautoservisas.github.io
---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.carousel img').each(function() {
+    var $src = $(this).attr('src');
+    var $color = $(this).attr('data-color');
+    $(this).parent().css({
+        'background-image': 'url(' + $src + ')',
+        'background-color': $color
+    });
+    $(this).remove();
+});
